aabfe1e7d8d8ed07f32855ebeebe908414f7aa79
Marko <teamsaboten.marko@gmail.com>
2016-01-26 02:19:18 +0900

    Removed comments

--- InsertionSort/main.cpp
+++ InsertionSort/main.cpp
@@ -12,7 +12,6 @@ int main() {
 	array[4] = 2;
 	array[5] = 1;
 
-	//My version
 	for (int j = 1; j < 6; j++) {
 		int tmp = array[j];
 		int i = j;
@@ -22,29 +21,6 @@ int main() {
 			i = i-1;
 		}
 	}
-	/*
-	//My old version
-		for (int i = 1 ; i < 6 ; i++) {
-			int j = i;
-			while (j >= 0 && array[j] < array[j-1]) {
-				int tmp = array[j-1];
-				array[j-1] = array[j];
-				array[j] = tmp;
-				j = j-1;
-			}
-		}
-
-	//Intro to Algo version
-	for (int j = 1 ; j < 6 ; j++) {
-		int key = array[j];
-		int i = j-1;
-		while (i >= 0 && array[i] > key) {
-			array[i+1] = array[i];
-			i = i-1;
-			array[i+1] = key;
-		}
-	}
-	*/
 
 	for (int i=0; i<6; i++)
 		std::cout << " " << array[i];
